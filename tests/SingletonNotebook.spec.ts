0b5c4a6407159fe27227766a5ed8fc8b5873eaa6
Diego Meza <diego.meza.ext@geodis.com>
2021-11-04 18:55:16 -0600

    Singleton tests added

---
+++ tests/SingletonNotebook.spec.ts
@@ -0,0 +1,30 @@
+import { SingletonNotebook } from '../src/proxy/SingletonNotebook';
+import { NoteProxy } from '../src/proxy/NoteProxy';
+import { expect } from 'chai';
+
+describe('NoteProxy', () => {
+  it('#new: Automaticly register itself updating the SingletonNotebook notes and dictionary when instanciated.', () => {
+    const note = new NoteProxy('Title', 'content');
+    expect(SingletonNotebook.dictionary.size).to.equal(1);
+    expect(SingletonNotebook.notes.size).to.equal(1);
+    // do it twice and it must keep the same values because the note has the same title.
+    const noteCopy = new NoteProxy('Title', 'content');
+    expect(SingletonNotebook.dictionary.size).to.equal(1);
+    expect(SingletonNotebook.notes.size).to.equal(1);
+  });
+  it('#update: Automaticly updates the SingletonNotebook notes and dictionary when updated.', () => {
+    const note = new NoteProxy('Title', 'content');
+    expect(SingletonNotebook.dictionary.size).to.equal(1);
+    expect(SingletonNotebook.notes.size).to.equal(1);
+    const noteUpdate = new NoteProxy('Title', 'content foo');
+    console.log(SingletonNotebook.dictionary.toArray());
+    expect(SingletonNotebook.dictionary.size).to.equal(2);
+    expect(SingletonNotebook.notes.size).to.equal(1);
+  });
+  // it('#update: Automaticly updates the SingletonNotebook notes and dictionary when updated.', () => {
+  //   const note = new NoteProxy('Title2', 'content');
+  //   expect(SingletonNotebook.dictionary.size).to.equal(2);
+  //   expect(SingletonNotebook.notes.size).to.equal(2);
+  // });
+});
+
